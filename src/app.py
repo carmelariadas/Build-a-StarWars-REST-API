10f445f82d5e67b371084d86aa197ac4ae210475
carmelariadas <carmen.mk.167@gmail.com>
2022-11-28 12:24:39 +0000

    faltan los favoritos

--- src/app.py
+++ src/app.py
@@ -2,13 +2,13 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, json
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters, Planets, Fav_Characters, Fav_Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,157 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+#***************** USERS *************************
+
+#ENDPOINT GET LOS USUARIOS CREADOS
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
+    users = User.query.all()
+    user_list = []
+    for user in users:
+        user_list.append(user.serialize())
+    return jsonify(user_list), 200
 
+#ENDPOINT GET UN USUARIO SEGÚN SU ID
+@app.route('/user/<int:id>', methods=['GET'])
+def get_user(id):
+    user = User.query.filter_by(id=id).one()
+    user = user.serialize()
+    return jsonify(user), 200
+
+
+#***************** CHARACTERS *************************
+
+#ENDPOINT GET CHARACTERS CREADOS
+@app.route('/people', methods=['GET'])
+def get_characters():
+    characters = Characters.query.all()
+    characters_list = []
+    for character in characters:
+        characters_list.append(character.serialize())
+    return jsonify(characters_list), 200
+
+#ENDPOINT GET UN CHARACTER SEGÚN SU ID
+@app.route('/people/<int:id>', methods=['GET'])
+def get_character(id):
+    character = Characters.query.filter_by(id=id).one()
+    character = character.serialize()
+    return jsonify(character), 200
+
+
+#ENDPOINT PARA CREAR UN CHARACTER (SIN USAR EL MENU DADO) / XEJ CON ESTO PUEDO AÑADIR UN CHARACTER DESDE POSTMAN
+@app.route('/people', methods=['POST'])
+def create_character():
+    data = request.data
+    data = json.loads(data)
+    character_new = Characters(
+        name = data["name"],
+        birthday_year = data["birthday_year"],
+        gender = data["gender"],
+        height = data["height"],
+        skin_color = data["skin_color"],
+        eye_color = data["eye_color"])
+    db.session.add(character_new)
+    db.session.commit()
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Añadido un personaje"
     }
-
     return jsonify(response_body), 200
+
+#ENDPOINT PARA MODIFICAR UN CHARACTER (SIN USAR EL MENU DADO) 
+@app.route('/people/<int:id>', methods=['PUT'])
+def modify_character(id):
+    data = request.data
+    data = json.loads(data)
+    character_modified = Characters.query.filter_by(id=id).one()
+    character_modified.skin_color = data["skin_color"]
+    character_modified.verified = True
+    db.session.commit()
+    response_body = {
+        "msg": "Personaje modificado"
+    }
+    return jsonify(response_body), 200
+
+
+#ENDPOINT PARA DELETE UN CHARACTER (SIN USAR EL MENU DADO) 
+@app.route('/people/<int:id>', methods=['DELETE'])
+def delete_character(id):
+    character_deleted = Characters.query.filter_by(id=id).one()
+    db.session.delete(character_deleted)
+    db.session.commit()
+    response_body = {
+        "msg": "Personaje borrado"
+    }
+    return jsonify(response_body), 200
+
+
+
+#***************** PLANETS *************************
+
+
+#ENDPOINT GET PLANETS CREADOS
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planets.query.all()
+    planets_list = []
+    for planet in planets:
+        planets_list.append(planet.serialize())
+    return jsonify(planets_list), 200
+
+#ENDPOINT GET UN PLANET SEGÚN SU ID
+@app.route('/planets/<int:id>', methods=['GET'])
+def get_planet(id):
+    planet = Planets.query.filter_by(id=id).one()
+    planet = planet.serialize()
+    return jsonify(planet), 200
+
+
+#ENDPOINT PARA CREAR UN PLANET (SIN USAR EL MENU DADO) 
+@app.route('/planets', methods=['POST'])
+def create_planet():
+    data = request.data
+    data = json.loads(data)
+    planet_new = Planets(
+        name = data["name"],
+        diameter = data["diameter"],
+        rotation_period = data["rotation_period"],
+        orbital_period = data["orbital_period"])
+    db.session.add(planet_new)
+    db.session.commit()
+    response_body = {
+        "msg": "Añadido un planet"
+    }
+    return jsonify(response_body), 200
+
+#ENDPOINT PARA MODIFICAR UN PLANET (SIN USAR EL MENU DADO) 
+@app.route('/planets/<int:id>', methods=['PUT'])
+def modify_planets(id):
+    data = request.data
+    data = json.loads(data)
+    planet_modified = Planets.query.filter_by(id=id).one()
+    planet_modified.diameter = data["diameter"]
+    planet_modified.verified = True
+    db.session.commit()
+    response_body = {
+        "msg": "Planet modificado"
+    }
+    return jsonify(response_body), 200
+
+#ENDPOINT PARA DELETE UN PLANET (SIN USAR EL MENU DADO) 
+@app.route('/planets/<int:id>', methods=['DELETE'])
+def delete_planet(id):
+    planet_deleted = Planets.query.filter_by(id=id).one()
+    db.session.delete(planet_deleted)
+    db.session.commit()
+    response_body = {
+        "msg": "Planet borrado"
+    }
+    return jsonify(response_body), 200
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,17 +3,103 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    lastname = db.Column(db.String(50), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Characters(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    birthday_year = db.Column(db.Integer, unique=False, nullable=False)
+    gender = db.Column(db.String(50), unique=False, nullable=False)
+    height = db.Column(db.Integer, unique=False, nullable=False)
+    skin_color = db.Column(db.String(50), unique=False, nullable=False)
+    eye_color = db.Column(db.String(50), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id, 
+            "name": self.name,
+            "birthday_year": self.birthday_year,
+            "gender": self.gender,
+            "height": self.height,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+        }
+
+
+
+class Planets (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    diameter = db.Column(db.Integer, unique=False, nullable=False)
+    rotation_period = db.Column(db.Integer, unique=False, nullable=False)
+    orbital_period = db.Column(db.Integer, unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id, 
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+        }
+
+class Fav_Characters (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id  = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    character = db.relationship(Characters)
+
+    def __repr__(self):
+        return '<Fav_Characters %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id, 
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+        }
+
+
+class Fav_Planets (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id  = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    planet = db.relationship(Planets)
+
+    def __repr__(self):
+        return '<Fav_Planets %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id, 
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+        }
+
